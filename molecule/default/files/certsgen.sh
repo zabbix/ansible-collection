04fa0b3db706ac667744fff476caca6d14491a4b
Andrey Tochko <andrejs.tocko@zabbix.com>
2023-04-28 19:09:11 +0300

    fixed cert tests

--- molecule/default/files/certsgen.sh
+++ molecule/default/files/certsgen.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 CANAME=root-ca
 CERTS=( root-ca $@ )
-
-# mkdir -p certs
+FOLDER="files/certs"
+mkdir -p $FOLDER
 
 if [[ " ${CERTS[*]} " =~ " ${CANAME} " ]]; then
 	echo Generate Root CA cert, key
 	# Create a key-pair that will serve both as the root CA and the server key-pair
 	# the "ca.crt" name is used to match what it expects later
-	openssl req -new -x509 -days 365 -nodes -out certs/ca.crt \
-		-keyout certs/ca.key -subj "/CN=$CANAME"
+	openssl req -new -x509 -days 365 -nodes -out $FOLDER/ca.crt \
+		-keyout $FOLDER/ca.key -subj "/CN=$CANAME"
 	
 	CERTS=( "${CERTS[@]/$CANAME}" )
 fi
 
 for CLIENT in "${CERTS[@]}"
 do
-	# Create the client key and CSR and sign with root key
-	openssl req -new -nodes -out certs/$CLIENT.csr \
-	  -keyout certs/$CLIENT.key -subj "/CN=$CLIENT"
+	if [ ! -z "$CLIENT" ]; then
+		# Create the client key and CSR and sign with root key
+		openssl req -new -nodes -out $FOLDER/$CLIENT.csr \
+		  -keyout $FOLDER/$CLIENT.key -subj "/CN=$CLIENT"
 
-	openssl x509 -req -in certs/$CLIENT.csr -days 365 \
-	    -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial \
-	    -out certs/$CLIENT.crt
+		openssl x509 -req -in $FOLDER/$CLIENT.csr -days 365 \
+			-CA $FOLDER/ca.crt -CAkey $FOLDER/ca.key -CAcreateserial \
+			-out $FOLDER/$CLIENT.crt
+	fi
 done
-chmod og-rwx certs/*
-
+chmod og-rwx $FOLDER/*
