b79af0a6c8a0ecb6d7d19f96adfae3829d3369d4
Andrey Tochko <andrejs.tocko@zabbix.com>
2023-05-03 10:45:26 +0300

    fixed test shell scripts

--- molecule/default/files/certsgen.sh
+++ molecule/default/files/certsgen.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 CANAME=root-ca
-CERTS=( root-ca $@ )
+CERTS=( "root-ca" "$@" )
 FOLDER="files/certs"
 mkdir -p $FOLDER
 
-if [[ " ${CERTS[*]} " =~ " ${CANAME} " ]]; then
+if [[ " ${CERTS[*]} " =~ ${CANAME} ]]; then
 	echo Generate Root CA cert, key
 	# Create a key-pair that will serve both as the root CA and the server key-pair
 	# the "ca.crt" name is used to match what it expects later
-	openssl req -new -x509 -days 365 -nodes -out $FOLDER/ca.crt \
-		-keyout $FOLDER/ca.key -subj "/CN=$CANAME"
+	openssl req -new -x509 -days 365 -nodes -out "$FOLDER/ca.crt" \
+		-keyout "$FOLDER/ca.key" -subj "/CN=$CANAME"
 	
 	CERTS=( "${CERTS[@]/$CANAME}" )
 fi
 
 for CLIENT in "${CERTS[@]}"
 do
-	if [ ! -z "$CLIENT" ]; then
+	if [ -n "$CLIENT" ]; then
 		# Create the client key and CSR and sign with root key
-		openssl req -new -nodes -out $FOLDER/$CLIENT.csr \
-		  -keyout $FOLDER/$CLIENT.key -subj "/CN=$CLIENT"
+		openssl req -new -nodes -out "$FOLDER/$CLIENT.csr" \
+		  -keyout "$FOLDER/$CLIENT.key" -subj "/CN=$CLIENT"
 
-		openssl x509 -req -in $FOLDER/$CLIENT.csr -days 365 \
-			-CA $FOLDER/ca.crt -CAkey $FOLDER/ca.key -CAcreateserial \
-			-out $FOLDER/$CLIENT.crt
+		openssl x509 -req -in "$FOLDER/$CLIENT.csr" -days 365 \
+			-CA "$FOLDER/ca.crt" -CAkey "$FOLDER/ca.key" -CAcreateserial \
+			-out "$FOLDER/$CLIENT.crt"
 	fi
 done
 chmod og-rwx $FOLDER/*
--- molecule/default/files/squid.sh
+++ molecule/default/files/squid.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 podman run -d --name squid-container -e TZ="Europe/Riga" -p 3128:3128 docker.io/ubuntu/squid
